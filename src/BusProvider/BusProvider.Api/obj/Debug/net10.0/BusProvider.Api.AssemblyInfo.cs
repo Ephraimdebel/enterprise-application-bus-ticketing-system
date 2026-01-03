dee36976825a4678644e231e0dcea7a67fa8ab97
Ephraim Debel <ephraimdebel@gmail.com>
2026-01-04 01:16:03 +0300

    fix

--- src/BusProvider/BusProvider.Api/obj/Debug/net10.0/BusProvider.Api.AssemblyInfo.cs
+++ src/BusProvider/BusProvider.Api/obj/Debug/net10.0/BusProvider.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusProvider.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9be3d0cc11b6b9f07bcb28c3739e8052280cd53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63c9e2345e38c36ff2105c31afe5e47eb2265b16")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusProvider.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusProvider.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/Dispute/Dispute.Domain/obj/Debug/net10.0/Dispute.Domain.AssemblyInfo.cs
+++ src/Dispute/Dispute.Domain/obj/Debug/net10.0/Dispute.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dispute.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63c9e2345e38c36ff2105c31afe5e47eb2265b16")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dispute.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dispute.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
