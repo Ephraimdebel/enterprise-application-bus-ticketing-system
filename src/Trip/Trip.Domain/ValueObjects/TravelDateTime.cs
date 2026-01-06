608035d404d8fe4eed4a6b38539002be33747778
Blen Redwan <blenredwan@gmail.com>
2026-01-06 14:01:54 +0300

    feat(trip): add value objects TravelDateTime and SeatNumber

---
+++ src/Trip/Trip.Domain/ValueObjects/TravelDateTime.cs
@@ -0,0 +1,25 @@
+namespace Trip.Domain.ValueObjects;
+
+public sealed class TravelDateTime : IEquatable<TravelDateTime>
+{
+    public DateOnly Date { get; }
+    public TimeOnly Time { get; }
+
+    public TravelDateTime(DateOnly date, TimeOnly time)
+    {
+        Date = date;
+        Time = time;
+    }
+
+    public bool Equals(TravelDateTime? other)
+    {
+        if (other is null) return false;
+        return Date == other.Date && Time == other.Time;
+    }
+
+    public override bool Equals(object? obj)
+        => Equals(obj as TravelDateTime);
+
+    public override int GetHashCode()
+        => HashCode.Combine(Date, Time);
+}
